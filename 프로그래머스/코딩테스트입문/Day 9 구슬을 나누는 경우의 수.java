bd3383277644fb8d8c27b5beb79115de96cb5aa3
Aox <92138800+Ssspil@users.noreply.github.com>
2023-01-17 16:16:59 +0900

    Update Day 9 구슬을 나누는 경우의 수.java

--- 프로그래머스/코딩테스트입문/Day 9 구슬을 나누는 경우의 수.java
+++ 프로그래머스/코딩테스트입문/Day 9 구슬을 나누는 경우의 수.java
@@ -12,3 +12,19 @@ class Solution {
         return result;
     }
 }
+
+
+/*
+    result 1
+    result = result * balls = 1 * 5 = 5
+    result = result / share = 5 / 1 = (5/1)
+    
+    result (5/1)
+    result = result * balls = (5/1) * 6 = 5 * 6
+    result = result / share = 5*6 / 1*2 = (5*6) / (1*2)
+
+    result ((5*6)/(1*2))
+    result = result * balls = ((5*6)/(1*2)) * 7 =  5 * 6 * 7
+    result = result / share = (5*6*7) / 1*2*3 = (5*6*7)/(1*2*3)
+
+*/
