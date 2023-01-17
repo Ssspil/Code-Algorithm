a422487919260425bf70b6d9cc59d71278004170
Aox <92138800+Ssspil@users.noreply.github.com>
2023-01-17 16:10:15 +0900

    Create Day 9 구슬을 나누는 경우의 수.java

---
+++ 프로그래머스/코딩테스트입문/Day 9 구슬을 나누는 경우의 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int balls, int share) {
+        int answer = combination(balls, share);
+        return answer;
+    }
+    
+    public int combination(int n, int m){
+        if(m == 0 || n == m){
+            return 1;
+        }
+        return combination(n-1, m-1) + combination(n-1, m);
+    }
+}
