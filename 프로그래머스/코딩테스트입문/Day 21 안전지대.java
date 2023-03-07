39ad081557d72062e80aedd2db384d21a05a67bc
Aox <92138800+Ssspil@users.noreply.github.com>
2023-03-08 00:25:32 +0900

    Update Day 21 안전지대.java

--- 프로그래머스/코딩테스트입문/Day 21 안전지대.java
+++ 프로그래머스/코딩테스트입문/Day 21 안전지대.java
@@ -1,47 +1,51 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[][] board) {
-        int answer = 0;
-
-        int boomExplosionRange = 1;
-
-        int[][] copyBoard = new int[board.length][board[0].length];
-
-        int tempIndex = 0;
-        for (int[] rows : board) {
-            copyBoard[tempIndex] = Arrays.copyOf(rows, board[tempIndex].length);
-            tempIndex++;
+    
+        int boardClone[][] = new int[board.length][board[0].length];
+        for(int i = 0; i < board.length; i++){ // 반복문 + ArrayCopy
+            System.arraycopy(board[i], 0, boardClone[i], 0, boardClone[0].length);
         }
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                int val = board[i][j];
-                if(val == 0) continue;
-
-                makeBoomArea(i, j, boomExplosionRange,copyBoard);
+        int row = board.length;
+        int col = board[0].length;
+        int count = 0;
+        
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                if(board[i][j] == 1){
+                    nonSafeZon(i, j, row, col, boardClone);
+                }
             }
         }
-
-        for (int[] ints : copyBoard) {
-            for (int anInt : ints) {
-                if(anInt == 0)
-                    answer++;
+        
+        
+        for(int k = 0; k < row; k++){
+            for(int p = 0; p < col; p++){
+                if(boardClone[k][p] == 0){
+                    count++;
+                }
             }
         }
-
-        return answer;
+        return count;
+        
     }
-
-    private void makeBoomArea(int row, int col, int boomExplosionRange, int[][] board) {
-        for (int r = row - boomExplosionRange; r <= row + boomExplosionRange ; r++) {
-            if(r < 0 || r >= board.length) continue;
-
-            for (int c = col - boomExplosionRange; c <= col + boomExplosionRange; c++) {
-                if(c < 0 || c >= board[0].length) continue;
-
-                board[r][c] = 1;
+    
+    // 3 2 5 5 
+    public static void nonSafeZon(int i, int j, int row, int col, int[][]boardClone){
+        
+        for(int i2 = i-1; i2 <= i+1; i2++ ){    // 2 3 4
+            if(i2 < 0 || i2 > row-1){
+                continue;
+            }
+            for(int j2 = j-1; j2 <= j+1; j2++){ // 1 2 3
+                if(j2 < 0 || j2 > col-1){
+                    continue;
+                }
+                boardClone[i2][j2] = 1;
             }
         }
+        
     }
+    
 }
